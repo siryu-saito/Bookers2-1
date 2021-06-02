1bce4609b478a51fd9e909e189d190b3e064725a
siryu-saito <saito.s.ryu@gmail.com>
2021-06-02 08:36:52 +0000

    [Fix]post_idからbook_idに変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,16 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   validates :name, uniqueness: true, length: { in: 2..20 }
   validates :introduction, length: { maximum: 50 }
-  
+
   has_many :books, dependent: :destroy
   has_many :favorites
   attachment :profile_image
-  
-  def liked_by?(post_id)
-    favorites.where(post_id: post_id).exists
+
+  def liked_by?(book_id)
+    favorites.where(book_id: book_id).exists?
   end
-  
+
 end
